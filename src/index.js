c29fde7585443cb2db1eed128491b9c2498ec6d7
Igor Lamas <igorlamas@fitec.org.br>
2022-10-19 20:22:06 -0300

    env port heroku

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ server.start().then(res => {
 })
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 // Now that our HTTP server is fully set up, we can listen to it.
 httpServer.listen(PORT, () => {
   console.log(`Server is now running on http://localhost:${PORT}/graphql`);
